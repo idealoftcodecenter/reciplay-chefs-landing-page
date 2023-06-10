3ee3c1d2e7d96f630d597cea9d67eefffd00dd68
Hrishikesh Joshi <Hrishi@idealoftstudio.com>
2023-06-10 16:04:40 +0530

    second update before all the changes in UI

--- public_html/js/other-pages.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($) {
-    'use strict';
-
-    $(window).scroll(function() {
-        if($(window).scrollTop() + $(window).height() == $(document).height()) {
-            alert("bottom!");
-        }
-    });
-});
